9a8ef04036e618a92280836395b46727ae0b4650
AndreiLeand <andreimenemedez@gmail.com>
2025-09-13 14:53:54 +0800

    UpdatedAct

--- MyFiles/Activity/activity11.py
+++ MyFiles/Activity/activity11.py
@@ -11,21 +11,21 @@
 
 temp = input("Input Temperature --> ")
 if int(temp.isnumeric()):
-    if temp <= 0:
+    if int(temp) <= 0:
         print("Your temperature is Below Freezing")
-    elif temp >=1 and temp <= 15:
+    elif int(temp) >=1 and int(temp) <= 15:
         print("Your temperature is Extreme Cold")
-    elif temp >= 16 and temp <= 30:
+    elif int(temp) >= 16 and int(temp) <= 30:
         print("Your temperature is Cold")
-    elif temp >= 31 and temp <= 38:
+    elif int(temp) >= 31 and int(temp) <= 38:
         print("Your Temperature is Lukewarm")
-    elif temp >= 39 and temp <= 42:
+    elif int(temp) >= 39 and int(temp) <= 42:
         print("Your temperature is Warm")
-    elif temp >= 43 and temp <= 50:
+    elif int(temp) >= 43 and int(temp) <= 50:
         print("Your temperature is Hot temp")
-    elif temp >= 51 and temp <= 60:
+    elif int(temp) >= 51 and int(temp) <= 60:
         print("Your temperature is Extremely Hot Temp")
-    elif temp >= 61:
+    elif int(temp) >= 61:
         print("Your temperature is Burning Temp")
 else:
     print("Invalid Temp")
