86159a37124df916e16d07f1d67bf908e0946c45
AndreiLeand <24.jpad@gmail.com>
2025-09-15 15:53:48 +0800

    Another Challenge in for loop

---
+++ MyFiles/Activity/activity14.py
@@ -0,0 +1,4 @@
+#DESCENDING ORDER LOOP
+
+for love in range(20,0,-1):
+    print(love)
---
+++ MyFiles/code_challenge6.py
@@ -0,0 +1,15 @@
+#ODD NUMBER SUMMATION
+#ENTER 7 RANDOM NUMBERS, GET THE SUMMATION OF ALL ODD NUMBERS GIVEN
+"""
+enter a number: 10
+enter a number: 4
+enter a number 3
+enter a number: 11
+enter a number : 84
+enter a number : 7
+enter a number : 1
+
+The sum
+
+
+"""
