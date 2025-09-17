3b461cf9dd17c49afb11e53b4eb5e9552d182b32
AndreiLeand <andreimenemedez@gmail.com>
2025-09-17 17:21:11 +0800

    for loop

---
+++ MyFiles/Code_challenge/MyFiles/Activity/activity12.py
@@ -0,0 +1,4 @@
+#Print my name 50 times
+
+for name in range(1,51,1):
+    print(f"{name}.Andrei Leand A Menemedez")
---
+++ MyFiles/Code_challenge/MyFiles/Activity/activity9.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+c = 6
+d = 7
+
+email = 'AndreiLeand'
+password = 'AlwaysHumble'
+
+print( email == 'AndreiLeand') and (password == 'AlwaysHumble')
+
+print(a > b) and (c > d)
+print(a > b) or (c > d)
+print(not(a > b) or ( c > d))
